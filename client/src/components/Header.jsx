755695040ee43c3c9bbd6f2af427ccfe5c4fd856
Viren Khatri <vk102002@gmail.com>
2023-11-18 19:14:07 +0530

    initial commit

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <div className="py-3 w-full bg-yellow-300 px-10 text-slate-800">
-      <h1 className="text-4xl font-bold">Keeper</h1>
+      <h1 className="text-4xl font-bold">NoteNest</h1>
     </div>
   );
 };
