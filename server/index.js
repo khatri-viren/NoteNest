82825babf095c52c907ac87bca120e1033e46b7b
Viren Khatri <vk102002@gmail.com>
2023-12-11 13:19:45 +0530

    vercel config

--- server/index.js
+++ server/index.js
@@ -29,7 +29,10 @@ const Note = mongoose.model("Note", noteSchema);
 app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
